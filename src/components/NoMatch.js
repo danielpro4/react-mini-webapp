e1b0bc821fe5f2c6596e7389928768e912a3d4fd
Daniel <daniel.perez.atanacio@gmail.com>
2021-05-31 19:07:40 -0500

    fix height of the NoMatch component

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+
 const NoMatch = ({ redirectTo = '/home/all' }) => {
     return (
         <section
             style={{
                 backgroundColor: 'white',
-                height: '80vh',
+                height: '90vh',
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
             }}
         >
-            <h2>Page not found</h2>
+            <h2>Page not found.</h2>
             <p>We couldn't find you are looking for.</p>
             <a href={redirectTo}>Go to home</a>
         </section>
